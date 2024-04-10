eae8c4638f9c0199ebad2fc15458d66dfbc34b43
Luis Nogoseke <luis.nogoseke@venkonetworks.com>
2024-04-10 20:15:23 -0300

    add REPL code

---
+++ src/repl.rs
@@ -0,0 +1,20 @@
+use std::io::{self, Write};
+
+use crate::lexer;
+use crate::token;
+
+pub fn start() {
+    let mut buffer = String::new();
+    loop {
+        buffer = String::new();
+        print!(">>>");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut buffer);
+        let mut l: lexer::Lexer = lexer::Lexer::new(buffer);
+        let mut tok = l.next_token();
+        while tok.Type != token::EOF {
+            println!("{}", tok);
+            tok = l.next_token();
+        }
+    }
+}
